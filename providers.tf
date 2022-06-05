000ce87653f1aa881c7986857a29ce73ed4203ba
Muhammad Shahed Aziz <shahed011@hotmail.com>
2022-06-05 14:11:54 +0100

    Clean up

--- providers.tf
+++ providers.tf
@@ -10,16 +10,10 @@ terraform {
 provider "aws" {
     region = "eu-west-2"
 
-    # default_tags {
-    #     Environment = "Test"
-    #     Owner = "Shahed"
-    # }
-}
-
-resource "aws_cloudwatch_log_group" "test-log-group" {
-  name = "test-log-group"
-
-  tags = {
-    Resource = "Test-LogGroup"
-  }
+    default_tags {
+        tags = {
+            Environment = "Test"
+            Owner = "Shahed"
+        }
+    }
 }
