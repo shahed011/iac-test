da91f7d034c250a108c31df8f3819e1d6c16c18e
Muhammad Shahed Aziz <shahed011@hotmail.com>
2022-06-05 13:12:49 +0100

    Clean up

--- providers.tf
+++ providers.tf
@@ -10,11 +10,6 @@ terraform {
 }
 
 provider "aws" {
-    # assume_role {
-    #   role_arn = "arn:aws:iam::385916149497:role/iac-test-spacelift"
-    #   session_name = "terraform"
-    # }
-
     region = "eu-west-2"
 
     default_tags {
